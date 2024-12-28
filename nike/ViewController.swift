708c790002a74c457a6cd405a504b925f807d1a7
group21 <group21@Users-Mac-mini-8.local>
2024-12-28 13:42:56 +0300

    Add splash screen

--- nike/ViewController.swift
+++ nike/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.goToMainScreen()
+        }
+    }
+
+    private func goToMainScreen() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // Идентификатор целевого View Controller'а
+        let mainVC = storyboard.instantiateViewController(withIdentifier: "WelcomeScreen")
+
+        // Установить его как rootViewController
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+        sceneDelegate?.window?.rootViewController = mainVC
+        sceneDelegate?.window?.makeKeyAndVisible()
+    }
+}
